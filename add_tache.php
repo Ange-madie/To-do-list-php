078ed293896831b044149e039bb421863f025527
Asolan <ggg1290@outlook.fr>
2024-04-02 19:29:12 +0100

    Réparation d'érreur dans add_tache.php

--- add_tache.php
+++ add_tache.php
@@ -13,9 +13,8 @@ $consiste_a = $_POST["but"];
 $dsn="mysql:dbname=".BASE.";host=".SERVER;
 try{
     $connexion = new PDO($dsn, USER, PASSWD);
-    $insertion = $connexion->exec("insert into tache(nom, categorie, faire_le, consiste_a) values ($name_tache, $categorie_tache, $faire_le, $categorie_tacheo)");
-    header("Location: http://localhost:8000");
-    exit();
+    $insertion = $connexion->exec("insert into tache(nom, categorie, faire_le, consiste_a) values ($name_tache, $categorie_tache, $faire_le, $consiste_a)");
+    echo "<h1>La tâche a été ajouter.</h1>";
 } catch (PDOException $e) {
     printf("Echec de la connexion : %s\n", $e->getMessage());
     exit();
