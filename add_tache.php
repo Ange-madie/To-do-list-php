ca337e823184d4407eca9d7751fe0d82803ec92c
Asolan <ggg1290@outlook.fr>
2024-04-04 20:39:31 +0100

    Résolution du problème avec mysql et commencement de l'onglet visualissation des tâches

--- add_tache.php
+++ add_tache.php
@@ -1,20 +1,28 @@
 <?php
+//asolan430@gmail.com
 define('USER',"root");
-define('PASSWD',"asolan430@gmail.com");
+define('PASSWD',"");
 define('SERVER',"localhost");
-define('BASE',"to_do_list");
+define('BASE',"to-do-list");
 
 
-$name_tache = isset($_POST["name"]) ?? "no-name";
-$categorie_tache = isset($_POST["categorie"]) ?? "no-categorie";
-$faire_le = isset($_POST["date"]) ?? "no-date";
-$consiste_a = isset($_POST["but"]) ?? "no-but";
+$name_tache = $_POST["name"] ?? "no-name";
+$categorie_tache = $_POST["categorie"] ?? "no-categorie";
+$faire_le = $_POST["date"] ?? "no-date";
+$consiste_a = $_POST["but"] ?? "no-but";
+
 
 $dsn="mysql:dbname=".BASE.";host=".SERVER;
 try{
     $connexion = new PDO($dsn, USER, PASSWD);
-    $insertion = $connexion->exec("insert into tache(nom, categorie, faire_le, consiste_a) values ($name_tache, $categorie_tache, $faire_le, $consiste_a)");
-    echo "<h1>La tâche a été ajouter.</h1>";
+    $query = $connexion->prepare("INSERT INTO taches(nom, categorie, faire_le, consiste_a) VALUES (:name_tache, :categorie_tache, :faire_le, :consiste_a)");
+    $query->bindParam(":name_tache", $name_tache);
+    $query->bindParam(":categorie_tache", $categorie_tache);
+    $query->bindParam(":faire_le", $faire_le);
+    $query->bindParam(":consiste_a", $consiste_a);
+    $confirm = $query->execute();
+    echo '<h1>La tâche a été créer<h1>';
+    echo '<a href="/">Revenir à l\'accueil.</a>';
 } catch (PDOException $e) {
     printf("Echec de la connexion : %s\n", $e->getMessage());
     exit();
--- view-tache.php
+++ view-tache.php
@@ -1,7 +1,27 @@
 <?php
 include "include/header.php";
+define('USER',"root");
+define('PASSWD',"");
+define('SERVER',"localhost");
+define('BASE',"to-do-list");
+$dsn="mysql:dbname=".BASE.";host=".SERVER;
 ?>
-
+<?php 
+$taches = new PDO("mysql:dbname=".BASE.";host=".SERVER, USER, PASSWD);
+try{
+    $connexion = new PDO($dsn, USER, PASSWD);
+    $resultat = $connexion->query("SELECT * FROM taches");
+    while($donnee = $resultat->fetch()){
+        echo "<h1>".$donnee[2]."</h1>";
+        echo "<p>".'La tâche "'.$donnee[1].'"'." est à faire le ".$donnee[3]." et consiste à ".$donnee[4]."</p>";
+    }
+    //echo $resultat->rowCount();
+    $resultat->closeCursor();
+} catch (PDOException $e) {
+    printf("Echec de la connexion : %s\n", $e->getMessage());
+    exit();
+}
+?>
 <?php
 include "include/footer.php";
 ?>
