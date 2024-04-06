ee049cbd47335687c2045bd2aabc26c033f261c4
Asolan <ggg1290@outlook.fr>
2024-04-06 13:20:03 +0100

    Configuration pour l'affichage des tâches version 1.1

--- view-tache.php
+++ view-tache.php
@@ -8,19 +8,21 @@ $dsn="mysql:dbname=".BASE.";host=".SERVER;
 ?>
 <?php 
 $taches = new PDO("mysql:dbname=".BASE.";host=".SERVER, USER, PASSWD);
-$categorie = "";
+$categories = array();
 try{
     $connexion = new PDO($dsn, USER, PASSWD);
     $resultat = $connexion->query("SELECT * FROM taches");
     while($donnee = $resultat->fetch()){
-        if($donnee[2]==$categorie){
-            echo "<p class=\"$categorie-tache\">".'La tâche "'.$donnee[1].'"'." est à faire le ".$donnee[3]." et consiste à ".$donnee[4]."</p>";
+        echo "test ".in_array($donnee[2], $categories);
+        if(in_array($donnee[2], $categories)){
+            echo "<p class=\"$donnee[2]-tache\">".'La tâche "'.$donnee[1].'"'." est à faire le ".$donnee[3]." et consiste à ".$donnee[4]."</p>";
         }else{
             echo "<div class=\"$donnee[2]\">";
             echo "<h1>".$donnee[2]."</h1>";
             echo "<p>".'La tâche "'.$donnee[1].'"'." est à faire le ".$donnee[3]." et consiste à ".$donnee[4]."</p>";
             echo "</div>";
         }
+        $categories[] = $donnee[2];
     }
     //echo $resultat->rowCount();
     $resultat->closeCursor();
